8879b1e1551653eed8fb0eeeebc31eefecba3dc5
ex-hibition <nozomumumu@gmail.com>
2019-09-15 00:56:45 +0900

    initial commit.

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, Blueprint, jsonify, request
+from flask_cors import CORS, cross_origin
+from heroes import Heroes
+
+hero = Blueprint('hero', __name__)
+
+"""一覧を返す"""
+@hero.route('/heroes', methods=['GET', ])
+@cross_origin()
+def get_heroes():
+    return jsonify(heroes.get_heroes()), 200
+
+
+"""名前を更新する"""
+@hero.route('/heroes', methods=['PUT', ])
+@cross_origin()
+def update_hero():
+    # 呼び出し元から引き渡されたjsonデータ
+    data = request.json
+    # データ更新
+    heroes.update_hero(data)
+    return jsonify(f"updated hero id={data.get('id')}"), 200
+
+
+"""ヒーローを追加する"""
+@hero.route('/heroes', methods=['POST'])
+@cross_origin()
+def add_hero():
+    # 呼び出し元から引き渡されたjsonデータ
+    data = request.json
+    # データ追加
+    result = heroes.add_hero(data)
+    return jsonify(result), 200
+
+
+"""id指定されたヒーローを返す"""
+@hero.route('/heroes/<target_id>', methods=['GET', 'POST'])
+@cross_origin()
+def get_hero(target_id):
+    return jsonify([x for x in heroes.get_heroes() if x['id'] == int(target_id)].pop()), 200
+
+
+if __name__ == '__main__':
+    heroes = Heroes()
+
+    app = Flask(__name__)
+    app.register_blueprint(hero, url_prefix='/angular')
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ heroes.py
@@ -0,0 +1,62 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Heroes:
+    # _unique_instance = None
+    #
+    # @classmethod
+    # def get_instance(cls) -> object:
+    #     if not cls._unique_instance:
+    #         cls._unique_instance = cls()
+    #     return cls._unique_instance
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.heroes = [
+            {"id": 11, "name": 'Dr Nice'},
+            {"id": 12, "name": 'Narco'},
+            {"id": 13, "name": 'Bombasto'},
+            {"id": 14, "name": 'Celeritas'},
+            {"id": 15, "name": 'Magneta'},
+            {"id": 16, "name": 'RubberMan'},
+            {"id": 17, "name": 'Dynama'},
+            {"id": 18, "name": 'Dr IQ'},
+            {"id": 19, "name": 'Magma'},
+            {"id": 20, "name": 'Tornado'},
+        ]
+
+    def get_heroes(self):
+        return self.heroes
+
+    def update_hero(self, hero):
+        for num, rec in enumerate(self.heroes):
+            self.logger.info(f"num={num}, rec={rec}")
+
+            if rec.get('id') == int(hero.get('id')):
+                # 指定されたidのデータを配列から削除
+                self.heroes.pop(num)
+                # 更新後データを配列に追加
+                self.heroes.append(hero)
+                break
+
+        self.logger.info(f"heroes={self.heroes}")
+
+    def add_hero(self, hero: dict):
+        # heroes.idの最大値を取得
+        max_id = 0
+        for rec in self.heroes:
+            if rec.get('id') > max_id:
+                max_id = rec.get('id')
+
+        # heroes.idの最大値+1をセット
+        hero.update(id=max_id + 1)
+
+        # 新規データを配列に追加
+        self.heroes.append(hero)
+
+        self.logger.info(f"new hero={hero}")
+        self.logger.info(f"heroes={self.heroes}")
+
+        return hero
